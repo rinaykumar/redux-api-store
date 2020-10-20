b523f29065cd54abfe0a4285838dc9bd5063331b
rinaykumar <rkumar4@mail.sfsu.edu>
2020-10-20 07:08:49 -0700

    HTML for ListingCreationForm

--- src/components/ListingCreationForm.js
+++ src/components/ListingCreationForm.js
@@ -3,6 +3,17 @@ import React from 'react';
 const ListingCreationForm = () => {
   return (
     <div>
+      <form>
+        <input type='text' id='input-title' placeholder='Title'/>
+        <br/>
+        <input type='text' id='input-type' placeholder='Type'/>
+        <br/>
+        <input type='text' id='input-price' placeholder='Price'/>
+        <br/>
+        <input type='text' id='input-description' placeholder='Description'/>
+        <br/>
+        <button type='submit' id='submit'>Submit</button>
+      </form>
     </div>
   );
 };
